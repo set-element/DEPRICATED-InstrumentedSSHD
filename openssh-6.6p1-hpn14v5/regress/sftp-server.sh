c7224f4a2b2813f4b6c249931d9a1feeefece9c5
scott campbell <set.element@gmail.com>
2014-10-21 11:19:55 -0400

    what?!?
    madness.  too much or too little coffee.

--- openssh-6.6p1-hpn14v5/regress/sftp-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /home/scottc/development/openssh-6.6p1/sftp-server -el debug3 -p realpath,stat,lstat 2>/home/scottc/development/openssh-6.6p1/regress/sftp-server.log
--- openssh-6.6p1-hpn14v5/regress/ssh-log-wrapper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /home/scottc/development/openssh-6.6p1/ssh -E/home/scottc/development/openssh-6.6p1/regress/ssh.log "$@"
